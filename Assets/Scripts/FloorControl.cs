dfab08c893b0d75738c14cb528e9fc21957e5643
chang47 <coralbue@gmail.com>
2017-01-21 12:49:02 -0800

    added floors

---
+++ Assets/Scripts/FloorControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorControl : MonoBehaviour {
+    public float speed = 0.05f;    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moving();
+	}
+
+    private void moving()
+    {
+        //this.transform.Translate(new Vector2(.1f, 0f));
+    }
+}
