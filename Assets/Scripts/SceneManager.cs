7ed37af785ff57451efce0738e88350b04278b2e
Andrew McPherson <mcphandr@amazon.com>
2017-01-21 13:01:14 -0800

    Changed createNewThug to createThug

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,48 +6,39 @@ public class SceneManager : MonoBehaviour {
 	//Declare prefabs (for assigning in inspector)
 	public GameObject Hero;
 	public GameObject Thug;
-	private ArrayList thugs;
 
 	//Declare the hero in our scene
+	private ArrayList thugs;
 	private Hero hero;
-
-	// Use this for initialization
+    
 	void Awake () {
 		thugs = new ArrayList ();
 
 		hero = this.createHero ();
-		this.createNewThug ();
-
-		hero.transform.parent = this.transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		this.createThug();
 	}
 
-	public void handleThugDeath(){
-		this.createNewThug ();
+	public void handleThugDeath() {
+		this.createThug();
 	}
 
-	public Hero getHero(){
+	public Hero getHero() {
 		return hero;
 	}
 
-	public ArrayList getThugs(){
+	public ArrayList getThugs() {
 		return thugs;
 	}
 
-	private GameObject createNewThug(){
+	private GameObject createThug() {
 		GameObject nextThug = Object.Instantiate(Thug, new Vector2(12f, 1.75f), Quaternion.identity);
 		nextThug.name = "Thug" + Random.Range(100, 999);
-		thugs.Add (nextThug);
+		thugs.Add(nextThug);
 		return nextThug;
 	}
 
 	private Hero createHero(){
 		GameObject heroObject = Instantiate(Hero, new Vector2(-7.5f, 1.75f), Quaternion.identity);
-		heroObject.transform.parent = this.transform;
 		return heroObject.GetComponent<Hero>();
 	}
 }
