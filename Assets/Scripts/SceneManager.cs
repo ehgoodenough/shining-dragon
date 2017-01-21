2bc579bfce67be68cf8b99ddbb8d59c35cb177fa
Andrew McPherson <mcphandr@amazon.com>
2017-01-21 11:50:26 -0800

    Added hero script to hero prefab.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -9,8 +9,7 @@ public class SceneManager : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		GameObject firstThug = Object.Instantiate(thug, new Vector2(12f, 1.75f), Quaternion.identity);
-		firstThug.transform.parent = this.transform;
-		firstThug.name = "Thug" + Random.Range (100, 999);
+		firstThug.name = "Thug" + Random.Range(100, 999);
 	}
 	
 	// Update is called once per frame
@@ -20,7 +19,6 @@ public class SceneManager : MonoBehaviour {
 
 	public void handleThugDeath(){
 		GameObject nextThug = Object.Instantiate(thug, new Vector2(12f, 1.75f), Quaternion.identity);
-		nextThug.transform.parent = this.transform;
 		nextThug.name = "Thug" + Random.Range(100, 999);
 	}
 }
