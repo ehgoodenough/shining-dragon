7d5680b85187cb5002f33e2ebb3d353a008e4bef
Timothy Moore <mtimothy983@gmail.com>
2017-01-21 14:34:45 -0800

    Health bar fadeout

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour {
     public int OriginalWidth; // TODO fetch this automatically
 
+    public GameObject HealthBarHolder;
+    public GameObject HPBarGlowEffectPreFab;
+
     public bool DebugHealthBar;
     public int DebugForcedHealth;
     public int DebugForcedHealthMax;
 
     private int StartingX;
 
+    private int LastWidth;
+
+    private RectTransform _RectTransform;
+
 	// Use this for initialization
 	void Start () {
         StartingX = (int)transform.position.x;
+        _RectTransform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -28,10 +36,35 @@ public class HealthBar : MonoBehaviour {
     {
         float percScale = health / (float)maxHealth;
 
-        var lossOfWidth = OriginalWidth * (1.0f - percScale);
-        var leftShift = lossOfWidth / 2;
-        transform.localScale = new Vector3(percScale, 1);
-        transform.position = new Vector3(StartingX - leftShift, transform.position.y);
+        var newWidth = Mathf.RoundToInt(OriginalWidth * percScale);
+        var lossOfWidth = OriginalWidth - newWidth;
+
+        if(lossOfWidth % 2 != 0)
+        {
+            return;
+        }
+
+        var leftShift = (int)(lossOfWidth / 2);
+        transform.position = new Vector3(StartingX - leftShift, transform.position.y, transform.position.z);
+        _RectTransform.sizeDelta = new Vector2(newWidth, _RectTransform.sizeDelta.y);
+
+        if(newWidth < LastWidth)
+        {
+            for(int offset = newWidth + 1; offset <= LastWidth; offset++)
+            {
+                var x = StartingX - (OriginalWidth / 2) + offset - 1;
+                SpawnGlowEffect(new Vector3(x, transform.position.y, 0));
+            }
+        }
+
+        LastWidth = newWidth;
+    }
+
+    private void SpawnGlowEffect(Vector3 position)
+    {
+        var glowEffect = Instantiate(HPBarGlowEffectPreFab, position, Quaternion.identity);
+        glowEffect.name = $"Glow Effect {position.x}";
+        glowEffect.transform.SetParent(HealthBarHolder.transform, true);
     }
 
     public void UpdateWidth (int health, int maxHealth)
