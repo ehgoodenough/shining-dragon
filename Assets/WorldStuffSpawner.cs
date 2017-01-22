4e843f332d664436790eecb31ba9e3ab488b9ecf
Timothy Moore <mtimothy983@gmail.com>
2017-01-21 21:18:47 -0800

    Add tree spawning automagically

---
+++ Assets/WorldStuffSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStuffSpawner : MonoBehaviour
+{
+    public GameObject TreePreFab;
+
+	void Start ()
+    {
+        SpawnTree(0);
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void SpawnTree (float x)
+    {
+        Instantiate(TreePreFab, new Vector2(x, TreePreFab.transform.position.y), Quaternion.identity);
+    }
+}
