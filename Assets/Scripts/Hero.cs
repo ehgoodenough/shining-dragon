58eb67e6a0cc9610c3bce2e1006909f648aef1cd
chang47 <coralbue@gmail.com>
2017-01-21 17:11:03 -0800

    reverted enemy damage

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -89,7 +89,7 @@ public class Hero : MonoBehaviour {
 	}
 
     public void beAttacked() {
-        this.health -= 30;
+        this.health -= 1;
         if(this.health <= 0) {
             manager.gameEnd(true);
             Debug.Log("You Win!!");
