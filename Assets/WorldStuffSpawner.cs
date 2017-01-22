014496849c3648d92bd79cf80d7b63073ccd92ec
Timothy Moore <mtimothy983@gmail.com>
2017-01-21 21:24:39 -0800

    add clouds

--- Assets/WorldStuffSpawner.cs
+++ Assets/WorldStuffSpawner.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class WorldStuffSpawner : MonoBehaviour
 {
     public GameObject TreePreFab;
+    public GameObject CloudsPreFab;
 
 	void Start ()
     {
         SpawnTree(0);
+        SpawnClouds(0); 
 	}
 	
 	void Update ()
@@ -18,6 +20,17 @@ public class WorldStuffSpawner : MonoBehaviour
 
     void SpawnTree (float x)
     {
-        Instantiate(TreePreFab, new Vector2(x, TreePreFab.transform.position.y), Quaternion.identity);
+        SpawnEnvironment(TreePreFab, x);
+    }
+
+    void SpawnClouds (float x)
+    {
+        SpawnEnvironment(CloudsPreFab, x);
+    }
+
+    void SpawnEnvironment (GameObject prefab, float x)
+    {
+        var obj = Instantiate(prefab, new Vector2(x, prefab.transform.position.y), Quaternion.identity);
+        obj.transform.parent = transform.parent;
     }
 }
